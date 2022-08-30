bd2267d2b022e610e1fc9e6001d43aff310b2cb3
AndrewMartial <andrewomosh44@gmail.com>
2022-08-30 22:31:58 +0300

    update api

--- src/app.js
+++ src/app.js
@@ -2,37 +2,36 @@
 // endpoints
 // const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1b59ecc8e3f24bdc800f1e087490230b';
 // const techUrl = 'https://inshorts.deta.dev/news?category=technology'
-const localUrl = "http://localhost:3000/articles"
+const localUrl = "http://localhost:3000/latests"
 
 // varriables
-let articles = document.getElementById("articles")
+
 
 
 // functions
-function getArticles() {
+function getLatest() {
 
     fetch(localUrl)
      .then((response) => response.json())
      .then(data => {
-        let output = ''
-        data.forEach(article => {
+        let output = '';
+        data.forEach(latest => {
 
             output += `
                 <div class="md:flex justify-bewtween">
                     <div class="bg-green-300 rounded-md mx-8 ">
                         <div class="w-1/2">
-                            <img class="w-1/2 rounded-lg" id="img-url" src="${article.urlToImage}" alt="">
+                            <img class="w-1/2 rounded-lg" id="img-url" src="${latest.urlToImage}" alt="">
                         </div>
                         <div class="p-4">
-                            <h3 class="text-lg font-semibold text-gray-800">${article.title}</h3>
-                            <li>${article.name}</li>
-                            <li>${article.author}</li>
-                            <li>${article.description}</li>
-                            <a class="text-red-500 text-sm font-medium" href="${article.url}">
+                            <h3 class="text-lg font-semibold text-gray-800">${latest.title}</h3>
+                            <h3>${latest.name}</h3>
+                            <h4>${latest.author}</h4>
+                            <p>${latest.description}</p>
+                            <p>${latest.content}</p>
+                            <a class="text-red-500 bg-green-400 text-sm font-medium" href="${latest.url}">
                                 Read more
                             </a>
-                            <li>${article.publishedAt}</li>
-                            <p>${article.content}</p>
                         </div>
                     </div>
                 </div>
@@ -49,7 +48,7 @@ function getArticles() {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    getArticles()
+    getLatest()
 
 
 })
