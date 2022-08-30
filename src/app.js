c55d1da66981b90e7b4447d274c68c6039ccc9d5
AndrewMartial <andrewomosh44@gmail.com>
2022-08-30 23:07:57 +0300

    test second local api

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 // endpoints
 // const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1b59ecc8e3f24bdc800f1e087490230b';
 // const techUrl = 'https://inshorts.deta.dev/news?category=technology'
-const localUrl = "http://localhost:3000/latests"
+const latestlUrl = "http://localhost:3000/latests"
+const localUrl = "http://localhost:3000/locals"
+
 
 // varriables
 
@@ -10,8 +12,7 @@ const localUrl = "http://localhost:3000/latests"
 
 // functions
 function getLatest() {
-
-    fetch(localUrl)
+    fetch(latestlUrl)
      .then((response) => response.json())
      .then(data => {
         let output = '';
@@ -45,10 +46,43 @@ function getLatest() {
 }
 
 
+function getLocal() {
+    fetch(localUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            let news = ''
+            data.forEach(local => {
+                
+                news += `
+                <div class="md:flex justify-bewtween">
+                    <div class="bg-green-300 rounded-md mx-8 ">
+                        <div class="w-1/2">
+                            <img class="w-1/2 rounded-lg" id="img-url" src="${local.urlToImage}" alt="">
+                        </div>
+                        <div class="p-4">
+                            <h3 class="text-lg font-semibold text-gray-800">${local.title}</h3>
+                            <h3>${local.name}</h3>
+                            <h4>${local.author}</h4>
+                            <p>${local.description}</p>
+                            <p>${local.content}</p>
+                            <a class="text-red-500 bg-green-400 text-sm font-medium" href="${local.url}">
+                                Read more
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                `
+            });
+            document.getElementById("news").innerHTML = news;
+
+        })
+
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    getLatest()
+    getLatest();
+    getLocal();
 
 
 })
